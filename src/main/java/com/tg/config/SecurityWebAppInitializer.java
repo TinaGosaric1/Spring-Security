6c82dccfe22f21262ee1e47e5353488ad2af7c9d
Tina Gosarič <tina.gosaric@gorenje.com>
2023-02-16 15:38:26 +0100

    Adding Custom Login Form

---
+++ src/main/java/com/tg/config/SecurityWebAppInitializer.java
@@ -0,0 +1,6 @@
+package com.tg.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/com/tg/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.tg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLogin")
+    public String showMyLoginPage() {
+        return "fancy-login";
+    }
+
+}
